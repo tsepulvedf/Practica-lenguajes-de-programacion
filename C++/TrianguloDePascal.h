ea5afea524e3289612a3684e76cf4373fcaa81cf
tsepulvedf <85259415+tsepulvedf@users.noreply.github.com>
2022-04-19 21:45:06 -0500

    Create TrianguloDePascal.h

---
+++ C++/TrianguloDePascal.h
@@ -0,0 +1,10 @@
+#ifndef C___TrianguloDePascal_H
+#define C___TrianguloDePascal_H
+
+class TrianguloDePascal{
+  public:
+    int ejecutarArchivo();
+    int imprimirArchivo();
+    TrianguloDePascal();
+};
+#endif //C___TrianguloDePascal_H
