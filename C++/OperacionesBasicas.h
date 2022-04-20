4b991f5286e3132bbc8f8e9e34a4b0be4a0f7d35
tsepulvedf <85259415+tsepulvedf@users.noreply.github.com>
2022-04-19 21:42:25 -0500

    Create OperacionesBasicas.h

---
+++ C++/OperacionesBasicas.h
@@ -0,0 +1,10 @@
+#ifndef C___OperacionesBasicas_H
+#define C___OperacionesBasicas_H
+
+class OperacionesBasicas{
+  public:
+    int ejecutarArchivo();
+    int imprimirArchivo();
+    OperacionesBasicas();
+};
+#endif //C___OperacionesBasicas_H
