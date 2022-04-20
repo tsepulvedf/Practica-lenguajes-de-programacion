d972e30611007ceb05b795d3d1022937d8bef383
tsepulvedf <85259415+tsepulvedf@users.noreply.github.com>
2022-04-19 21:38:10 -0500

    Create FactorialJCoCo.h

---
+++ C++/FactorialJCoCo.h
@@ -0,0 +1,15 @@
+#ifndef C___FactorialJCoCo_H
+#define C___FactorialJCoCo_H
+
+class FactorialJCoCo{
+  public:
+    int ejecutarArchivo();
+    int imprimirArchivo();
+    FactorialJCoCo();
+
+
+};
+
+
+
+#endif //C___FactorialJCoCo_H
